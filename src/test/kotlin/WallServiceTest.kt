d4e2635a1372adeb41c1b0bf7e289f7ae544eae3
Artem <artem.verbicky1991@gmail.com>
2021-03-16 19:28:16 +0300

    hometask 3

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -5,7 +5,7 @@ import org.junit.Assert.*
 class WallServiceTest {
 
     @Test
-    fun update() {
+    fun update_caseTrue() {
         //arrange
         val service = WallService()
 
@@ -19,6 +19,28 @@ class WallServiceTest {
 
         //act
         val update = Post()
+        val result = service.update(update)
+
+        //assert
+        assertTrue(result)
+
+    }
+
+    @Test
+    fun update_caseFalse() {
+        //arrange
+        val service = WallService()
+
+        val post1 = Post(id = 1)
+        val post2 = Post(id = 2)
+        val post3 = Post(id = 3)
+
+        service.add(post1)
+        service.add(post2)
+        service.add(post3)
+
+        //act
+        val update = Post(id = 4)
         val result = service.update(update)
 
         //assert
